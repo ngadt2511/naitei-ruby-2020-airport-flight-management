5c062d02edb03801612b823a5d26b1184721ee6b
ngadt2511 <do.thuy.nga@sun-asterisk.com>
2020-08-17 11:17:36 +0700

    Refs task/27030 [user] logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,11 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
   private
 
   def remember_user
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     get "users/new"
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
     resources :users, only: :show
   end
 end
